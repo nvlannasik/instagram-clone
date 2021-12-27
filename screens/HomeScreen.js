040571cf956ab9df41f9dd7f1b3164152a74941c
nvlannasik <annasikn@yahoo.com>
2021-12-27 19:55:42 +0700

    add component stories

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 import Header from '../component/home/Header';
+import Stories from '../component/home/Stories';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
+      <Stories />
     </SafeAreaView>
   );
 };
