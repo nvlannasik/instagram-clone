79ae21b113971b3df25472167812b6f2565ecbf2
nvlannasik <annasikn@yahoo.com>
2021-12-27 21:31:06 +0700

    add dummy data for post

---
+++ data/posts.js
@@ -0,0 +1,37 @@
+import {USERS} from './user';
+export const POSTS = [
+  {
+    imageUrl: 'https://adaptivenetworklab.org/img/team/Pieter.png',
+    user: USERS[0].user,
+    likes: 9292,
+    caption: 'lololololololo',
+    profilePicture: USERS[0].image,
+    comments: [
+      {
+        user: 'kwkwkwkwk',
+        comment: 'wiwiwiwiwiwiwiwekeikwijeiwjeijrifewuiohwfeiouewf lorem',
+      },
+      {
+        user: 'kwkwkwkwk',
+        comment: 'wiwiwiwiwiwiwiwekeikwijeiwjeijrifewuiohwfeiouewf lorem',
+      },
+    ],
+  },
+  {
+    imageUrl: 'https://adaptivenetworklab.org/img/team/Pieter.png',
+    user: USERS[0].user,
+    likes: 9292,
+    caption: 'lololololololo',
+    profilePicture: USERS[0].image,
+    comments: [
+      {
+        user: 'kwkwkwkwk',
+        comment: 'wiwiwiwiwiwiwiwekeikwijeiwjeijrifewuiohwfeiouewf lorem',
+      },
+      {
+        user: 'kwkwkwkwk',
+        comment: 'wiwiwiwiwiwiwiwekeikwijeiwjeijrifewuiohwfeiouewf lorem',
+      },
+    ],
+  },
+];
