858028c9a61bc37f12bf7c8cfea2fd3902ef4a06
nvlannasik <annasikn@yahoo.com>
2021-12-31 01:11:58 +0700

    add firestore for input data signup

--- component/signUpScreen/SignUpForm.js
+++ component/signUpScreen/SignUpForm.js
@@ -12,6 +12,7 @@ import {Formik} from 'formik';
 import * as Yup from 'yup';
 import Validator from 'email-validator';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 const SignupForm = ({navigation}) => {
   const SignupFormSchema = Yup.object().shape({
@@ -21,10 +22,30 @@ const SignupForm = ({navigation}) => {
       .required()
       .min(6, 'Your password has to have at least 6 characters'),
   });
-  const onSignup = async (email, password) => {
+
+  const getRandomProfilePicture = async () => {
+    const response = await fetch('https://randomuser.me/api');
+    const data = await response.json();
+    return data.results[0].picture.large;
+  };
+
+  const onSignup = async (email, password, username) => {
     try {
-      await auth().createUserWithEmailAndPassword(email, password);
-      console.log('Firebase user created sukses ', email, password);
+      const authUser = await auth().createUserWithEmailAndPassword(
+        email,
+        password,
+        username,
+      );
+      console.log('Firebase user created sukses ', email, password, username);
+
+      firestore()
+        .collection('Users')
+        .add({
+          owner_uid: authUser.user.uid,
+          username: username,
+          email: authUser.user.email,
+          profile_picture: await getRandomProfilePicture(),
+        });
     } catch (error) {
       Alert.alert('gagal buat');
     }
@@ -34,7 +55,7 @@ const SignupForm = ({navigation}) => {
       <Formik
         initialValues={{email: '', username: '', password: ''}}
         onSubmit={values => {
-          onSignup(values.email, values.password);
+          onSignup(values.email, values.password, values.username);
         }}
         validationSchema={SignupFormSchema}
         validateOnMount={true}>
